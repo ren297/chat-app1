5a664c7ffa613f5f41d8fd9830cf100dd062dc0e
ren297 <93453210+ren297@users.noreply.github.com>
2021-12-05 18:02:31 +0900

    roomテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_05_082823) do
+ActiveRecord::Schema.define(version: 2021_12_05_090100) do
+
+  create_table "rooms", charset: "utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
