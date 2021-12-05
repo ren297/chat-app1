72f6f48d2d6a8e0c5e73e3d3068ba2c4ccb5775e
ren297 <93453210+ren297@users.noreply.github.com>
2021-12-05 18:22:57 +0900

    画面遷移の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,21 +1,24 @@
 class RoomsController < ApplicationController
-  class RoomsController < ApplicationController
-    def new
-      @room = Room.new
-    end
   
-    def create
-      @room = Room.new(room_params)
-      if @room.save
-        redirect_to root_path
-      else
-        render :new
-      end
-    end
+  def index
+  end
+
+  def new
+    @room = Room.new
+  end
   
-    private
-  
-    def room_params
-      params.require(:room).permit(:name, user_ids: [])
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
     end
   end
+  
+  private
+  
+  def room_params
+    params.require(:room).permit(:name, user_ids: [])
+  end
+end
